af2a578774b2748ce15be43447077fb9c90b36c1
Kagana1212 <ktc2243030004@edu.kyoto-tech.ac.jp>
2024-08-24 05:00:24 +0000

    test

---
+++ public/phptest.php
@@ -0,0 +1,42 @@
+<?php
+
+print('<h1>現在日時は</h1>');
+date_default_timezone_set('Asia/Tokyo');
+print(date('Y年 m月 d日 G時 i分 s秒'));
+
+
+
+
+<svg
+  width="300"
+  height="500"
+  viewBox="0 0 1000 1800"
+  xmlns="http://www.w3.org/2000/svg"
+>
+<circle cx="500" cy="1000" r="500" fill="white" stroke="black"
+/>
+<line x1="500" y1="1000" x2="500" y2="1300" stroke="black" stroke-width="6" stroke-linecap="round" />
+<line x1="500" y1="1000" x2="50" y2="1000" stroke="black" stroke-width="6" stroke-linecap="round"/>
+<line x1="500" y1="1000" x2="200" y2="700" stroke="black" stroke-width="4" stroke-linecap="round"/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</svg>
+
+
+
+
+
+
